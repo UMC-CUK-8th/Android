2cc0f53e7ad5216c233122f71eb17be27a19eb3b
jsh424 <jsh911424@catholic.ac.kr>
2025-03-23 01:42:20 +0900

    Week1/Jack
    최종

---
+++ Week1/app/src/main/java/com/example/week1/AnxiousActivity.kt
@@ -0,0 +1,14 @@
+package com.example.week1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AnxiousActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anxious) // XML로 화면 구성
+    }
+}
