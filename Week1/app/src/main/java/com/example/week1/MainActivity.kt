d60c31219e4bedb092c90bf5d6ff1c790b078e84
Bazzi331 <idonghwa0301@gmail.com>
2025-03-23 21:22:39 +0900

    Week1-2_Bazzi

--- Week1/app/src/main/java/com/example/week1/MainActivity.kt
+++ Week1/app/src/main/java/com/example/week1/MainActivity.kt
@@ -1,48 +1,19 @@
 package com.example.week1
 
+import android.content.Intent              // ← 인텐트 import!
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.week1.ui.theme.Week1Theme
+import android.view.View                 // ← View import도 필요!
+import androidx.appcompat.app.AppCompatActivity
 
-
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            Week1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Week1Theme {
-        Greeting("Android")
+    // 💡 버튼 클릭 시 호출될 함수
+    fun goToYellow(view: View) {
+        val intent = Intent(this, YellowActivity::class.java)
+        startActivity(intent)
     }
 }
