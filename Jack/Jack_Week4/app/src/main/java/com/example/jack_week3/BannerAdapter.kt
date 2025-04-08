452c9da6f74bdea4be4f8951bac1379253a155e4
jsh424 <jsh911424@catholic.ac.kr>
2025-04-08 19:56:57 +0900

    Jack/Week4
    3

---
+++ Jack/Jack_Week4/app/src/main/java/com/example/jack_week3/BannerAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.jack_week3
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+
+class BannerAdapter(private val bannerList: List<BannerItem>) :
+    RecyclerView.Adapter<BannerAdapter.BannerViewHolder>() {
+
+    class BannerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val bannerImage: ImageView = itemView.findViewById(R.id.bannerImage)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BannerViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_banner, parent, false)
+        return BannerViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: BannerViewHolder, position: Int) {
+        val bannerItem = bannerList[position]
+        holder.bannerImage.setImageResource(bannerItem.imageRes)
+    }
+
+    override fun getItemCount(): Int = bannerList.size
+}
