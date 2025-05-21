f24386d8b23e35851b66f401f748f3664ac9a129
jsh424 <jsh911424@catholic.ac.kr>
2025-05-21 09:14:07 +0900

    Jack/Week8
    1

--- Jack/Jack_Week4/app/src/main/java/com/example/jack_week4/LibraryFragment.kt
+++ Jack/Jack_Week4/app/src/main/java/com/example/jack_week4/LibraryFragment.kt
@@ -1,16 +1,18 @@
 package com.example.jack_week4
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.jack_week4.databinding.FragmentLibraryBinding
 import com.google.android.material.tabs.TabLayoutMediator
 
 class LibraryFragment : Fragment() {
     lateinit var binding: FragmentLibraryBinding
-    private val information = arrayListOf("저장한곡", "음악파일")
+    private val information = arrayListOf("저장한곡", "음악파일","저장앨범")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,7 +32,47 @@ class LibraryFragment : Fragment() {
             bottomSheetFragment.show(requireFragmentManager(), "BottomSheetDialog")
         }
 
+        binding.libraryLoginTv.setOnClickListener{
+            startActivity(Intent(activity, LoginActivity::class.java))
+        }
+
         return binding.root
     }
 
+    override fun onStart(){
+        super.onStart()
+        initViews()
+    }
+
+    private fun getJwt(): Int {
+        val spf = activity?.getSharedPreferences("auth" , AppCompatActivity.MODE_PRIVATE)
+
+        return spf!!.getInt("jwt", 0)
+    }
+    private fun initViews() {
+        val jwt: Int = getJwt()
+
+        if (jwt == 0){
+            binding.libraryLoginTv.text = "로그인"
+
+            binding.libraryLoginTv.setOnClickListener {
+                startActivity(Intent(activity, LoginActivity::class.java))
+            }
+        }
+        else{
+            binding.libraryLoginTv.text = "로그아웃"
+
+            binding.libraryLoginTv.setOnClickListener {
+                logout()
+                startActivity(Intent(activity, MainActivity::class.java))
+            }
+        }
+    }
+    private fun logout() {
+        val spf = activity?.getSharedPreferences("auth" , AppCompatActivity.MODE_PRIVATE)
+        val editor = spf!!.edit()
+
+        editor.remove("jwt")
+        editor.apply()
+    }
 }
