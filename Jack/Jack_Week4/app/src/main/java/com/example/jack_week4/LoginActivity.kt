57636df2560ebdcd3d34495c777f1cd8fa003895
jsh424 <jsh911424@catholic.ac.kr>
2025-05-28 09:01:09 +0900

    Jack/Week9
    1

--- Jack/Jack_Week4/app/src/main/java/com/example/jack_week4/LoginActivity.kt
+++ Jack/Jack_Week4/app/src/main/java/com/example/jack_week4/LoginActivity.kt
@@ -4,13 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.jack_week4.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), LoginView {
     lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,17 +17,15 @@ class LoginActivity : AppCompatActivity() {
 
         binding.loginSignUpTv.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
-
         }
+
         binding.loginSignInBtn.setOnClickListener {
             login()
         }
     }
 
     private fun login() {
-        if (binding.loginIdEt.text.toString()
-                .isEmpty() || binding.loginDirectInputEt.text.toString().isEmpty()
-        ) {
+        if (binding.loginIdEt.text.toString().isEmpty() || binding.loginDirectInputEt.text.toString().isEmpty()) {
             Toast.makeText(this, "이메일을 입력해주세요.", Toast.LENGTH_SHORT).show()
             return
         }
@@ -40,31 +35,47 @@ class LoginActivity : AppCompatActivity() {
             return
         }
 
-        val email =
-            binding.loginIdEt.text.toString() + "@" + binding.loginDirectInputEt.text.toString()
+        val user = getUser()
+        Log.d("LOGIN-REQUEST", "email: ${user.email}, password: ${user.password}")
+
+        val authService = AuthService()
+        authService.setLoginView(this)
+        authService.login(user)
+    }
+
+    private fun getUser(): User {
+        val email = binding.loginIdEt.text.toString() + "@" + binding.loginDirectInputEt.text.toString()
         val password = binding.loginPasswordEt.text.toString()
 
-        val songDB = SongDatabase.getInstance(this)!!
-        val user = songDB.userDao().getUser(email, password)
-
-        if (user != null) {
-            Log.d("LOGIN_ACT/GET_USER", "userId: ${user.id}, $user")
-            saveJwt(user.id)
-            startMainActivity()
-        } else {
-            Toast.makeText(this, "회원 정보가 존재하지 않습니다.", Toast.LENGTH_SHORT).show()
-        }
+        Log.d("LOGIN-EMAIL", "Email: $email, Password: $password")
+        return User(email = email, password = password, name = "")
     }
 
-    private fun saveJwt(jwt: Int) {
-        val spf = getSharedPreferences("auth", MODE_PRIVATE)
-        val editor = spf.edit()
 
-        editor.putInt("jwt", jwt)
-        editor.apply()
-    }
     private fun startMainActivity() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun saveJwt2(jwt: String) {
+        val spf = getSharedPreferences("auth2", MODE_PRIVATE)
+        val editor = spf.edit()
+        editor.putString("jwt", jwt)
+        editor.apply()
+    }
+
+    override fun onLoginSuccess(code: String, result: Result) {
+        when (code) {
+            "200" -> {
+                saveJwt2(result.accessToken)
+                Log.d("LOGIN-ACT/JWT", result.accessToken)
+                startMainActivity()
+            }
+        }
+    }
+
+    override fun onLoginFailure() {
+        Log.d("LOGIN-FAILURE", "로그인 실패")
+        Toast.makeText(this, "로그인에 실패했습니다.", Toast.LENGTH_SHORT).show()
     }
 }
